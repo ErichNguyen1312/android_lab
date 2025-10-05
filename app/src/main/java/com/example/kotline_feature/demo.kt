c304f02fdb495009d03e4024022d836e889b13da
ErichNguyen1312 <vinhnguyenfit1312@gmail.com>
2025-10-05 15:05:44 +0700

    init project for android case study

---
+++ app/src/main/java/com/example/kotline_feature/demo.kt
@@ -0,0 +1,17 @@
+package com.example.kotline_feature
+
+
+fun  main(){
+    println("Demo kotlin")
+
+    //declare variables
+    //val: read only, var: mutable
+    val popcorn =5;
+    val hotdogs =7;
+    var customer =10;
+
+    //its value can be reassigned after declaration
+    customer = 8;
+
+    println(customer)
+}
