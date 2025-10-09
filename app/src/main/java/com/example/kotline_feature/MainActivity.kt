ed7b16aa18b9de2027fc2ad8cf214bc85e2c5410
ErichNguyen1312 <vinhnguyenfit1312@gmail.com>
2025-10-09 15:51:36 +0700

    syntax basic

--- app/src/main/java/com/example/kotline_feature/MainActivity.kt
+++ app/src/main/java/com/example/kotline_feature/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             Kotline_featureTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "skype blue",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
--- app/src/main/java/com/example/kotline_feature/demo.kt
+++ app/src/main/java/com/example/kotline_feature/demo.kt
@@ -1,17 +1,67 @@
 package com.example.kotline_feature
 
+import kotlin.reflect.typeOf
+
 
 fun  main(){
     println("Demo kotlin")
 
+    var age : Int =0;
+
+    println("Khai bao do tuoi: ")
+    age = readln().toInt()
+
+    // in keyword
+    if( age in  18.. 39) {
+        println("You can go in")
+    }else if(age >=40){
+        println("You are too old")
+    }
+    else {
+        println("You can't go in")
+    }
+
     //declare variables
     //val: read only, var: mutable
-    val popcorn =5;
-    val hotdogs =7;
-    var customer =10;
+//    val popcorn =5;
+//    val hotdogs =7;
+//
+//    var customer =10;
+//
+//    //its value can be reassigned after declaration
+//    customer = 8;
+//
+//    println(customer)
+//
+//    println("There are $customer customer")
+//
+//    println("There are ${customer+1} customer")
+//
+//    //collections
+//
+//    println("Collections")
+//    // read only list
+//    val listReadOnly = listOf("1","2","3","4")
+//    println(listReadOnly)
+//    println(listReadOnly.first())
+//    println(listReadOnly.last())
+//
+//    println(listReadOnly.count());
+//
+//    println("check number 6 in list "+ "6" in listReadOnly )
 
-    //its value can be reassigned after declaration
-    customer = 8;
+//    val  circleRatio = 3.1415926535F
+//    println(circleRatio)
 
-    println(customer)
+//    val myTrue: Boolean = true
+//    val myFalse: Boolean = false
+//    val boolNull: Boolean? = null
+
+//    println(myTrue && myTrue)
+//
+//
+//    var name : String = "Demo"
+//    name = "Setting up your Java environment"
+//
+//    println(name.length)
 }
