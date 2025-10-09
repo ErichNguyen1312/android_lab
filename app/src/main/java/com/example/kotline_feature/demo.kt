9fcec29faf11342e1e17342dbae7aa10a16e2e19
ErichNguyen1312 <vinhnguyenfit1312@gmail.com>
2025-10-10 01:14:18 +0700

    mini app paper rock scissors

--- app/src/main/java/com/example/kotline_feature/demo.kt
+++ app/src/main/java/com/example/kotline_feature/demo.kt
@@ -2,16 +2,54 @@ package com.example.kotline_feature
 
 
 fun  main(){
-    println("Demo kotlin")
+    println("Rock paper scissors: ")
+    var playerChoice = ""
+    var computerChoice = ""
 
-    //declare variables
-    //val: read only, var: mutable
-    val popcorn =5;
-    val hotdogs =7;
-    var customer =10;
+    println("Enter you choice: ")
+    playerChoice= readln()
 
-    //its value can be reassigned after declaration
-    customer = 8;
+    val randomNumber = (1..3).random()
 
-    println(customer)
+//    if(randomNumber == 1) {
+//        computerChoice = "Rock"
+//    } else if(randomNumber ==2){
+//        computerChoice = "Paper"
+//    }else{
+//        computerChoice = "Scissors"
+//    }
+
+    //using when statement
+    when (randomNumber) {
+        1-> computerChoice = "Rock"
+        2-> computerChoice = "Paper"
+        3-> computerChoice = "Scissors"
+
+    }
+
+    //find out winner
+//    var winner = when{
+//        playerChoice == computerChoice ->"Tie"
+//        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+//        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+//        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+//        else -> "Computer"
+//    }
+
+    //using when Expression
+
+    var winner =when{
+        playerChoice == computerChoice ->"Tie"
+        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        else -> "Computer"
+    }
+
+
+    println("Player choice: "+playerChoice)
+    println("Computer choice: "+computerChoice)
+
+    println(winner + " Winner")
+
 }
