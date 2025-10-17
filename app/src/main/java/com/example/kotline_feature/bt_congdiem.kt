38ce83bcf25638feb6d1be9c414a975f77611769
ErichNguyen1312 <vinhnguyenfit1312@gmail.com>
2025-10-18 03:41:43 +0700

    nothing

--- app/src/main/java/com/example/kotline_feature/bt_congdiem.kt
+++ app/src/main/java/com/example/kotline_feature/bt_congdiem.kt
@@ -3,5 +3,15 @@ package com.example.kotline_feature
 fun main(){
     println("Nhom 12 - Nguyen Dang Vinh")
 
+    println("Nhap so luong sinh vien: ")
+    var n= readln().toInt()
+    println("Nhap danh sach sinh vien: ")
+    var danhSach = mutableListOf<String>()
+
+    while (true){
+        println("Nhap sinh vien: ")
+        var sinhVien = readln()
+        
+    }
 
 }
